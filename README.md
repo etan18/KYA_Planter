20b6d40594ebcd95cbc550f6504eb52bf2aae33e
etan18 <erintan4@gmail.com>
2020-12-04 09:52:38 -0500

    create main python file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # KYA_Planter
+
+Application written by Erin Tan and Meha Patel of the Middlesex County Academy for Science, Mathematics, and Engineering Technologies as part of the Capstone Project
--- main.py
+++
@@ -1,5 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.screenmanager import ScreenManager, Screen
