389c705727c82af4dd8ce9d855c838d3c2a883a8
mxp03 <patelmeha03@gmail.com>
2021-01-08 18:53:16 -0500

    meha app basic stuff

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
 import NumPy
 import clr
 import System
+from kivy.app import App
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+
+fburl = string(input("Enter Firebase Database URL"))
+
+ print ("1 = Aster")
+ print ("2 = Iris")
+ print ("3 = Marsh Marigold")
+
+ plantype = int(input("Choose Plant Type: "))
+ print("You chose ")
+ if (plantype == 1):
+     {
+     print("Aster")
+     int x = .3
+     }
+if (plantype == 2):
+    {
+    print("Iris")
+    int x = .5
+    }
+if (plantype == 3):
+    {
+    print("Marsh Marigold")
+    int x = .7
+    }
+
+print("x = ") << print(x)
