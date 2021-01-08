7d4dbfd7f2b763b02b3348b1bd13523ebae45a32
etan18 <erintan4@gmail.com>
2021-01-08 10:53:12 -0500

    prep for demo

--- valve.py
+++ valve.py
@@ -5,6 +5,10 @@ valve_pin = 4
 
 def waterPlant(threshold):
     GPIO.output(valve_pin, GPIO.HIGH)
-    sleep(5)
+    for x in range(4):
+        print("watering plant...")
+        sleep(1)
+    print("closing valve...")
     GPIO.output(valve_pin, GPIO.LOW)
+    sleep(10)
     return
