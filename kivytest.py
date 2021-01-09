114f1397c7a3ef9b2651d035b753c573035e0646
mxp03 <patelmeha03@gmail.com>
2021-01-09 18:22:27 -0500

    kt

--- kivytest.py
+++ kivytest.py
@@ -6,12 +6,13 @@ from kivy.uix.image import AsyncImage
 from kivy.uix.behaviors import ButtonBehavior
 
 class MainApp(App):
-     def build(self):
+    def build(self):
         img = Image(source='/path/to/real_python.png',
                     size_hint=(1, .5),
                     pos_hint={'center_x':.5, 'center_y':.5})
 
         return img
+
 if __name__ == '__main__':
     app = MainApp()
     app.run()
