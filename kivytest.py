b989dc3f2cc3dc75a29912f69f39eec4850f7748
mxp03 <patelmeha03@gmail.com>
2021-01-09 18:20:03 -0500

    kivytest

--- kivytest.py
+++ kivytest.py
@@ -1,14 +1,17 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import AsyncImage
+from kivy.uix.behaviors import ButtonBehavior
 
 class MainApp(App):
-    def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+     def build(self):
+        img = Image(source='/path/to/real_python.png',
+                    size_hint=(1, .5),
+                    pos_hint={'center_x':.5, 'center_y':.5})
 
-        return label
-
+        return img
 if __name__ == '__main__':
     app = MainApp()
     app.run()
