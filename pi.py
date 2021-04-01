38e2725a29b7f87a92890ddcb1c4ebb9bfceb322
etan18 <erintan4@gmail.com>
2021-03-31 22:13:36 -0400

    ;

--- pi.py
+++ pi.py
@@ -18,7 +18,7 @@ def waterPlant():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(VALVE_PIN, GPIO.OUT)
     GPIO.output(VALVE_PIN, GPIO.HIGH)
-    time.sleep(10)
+    time.sleep(15)
     GPIO.output(VALVE_PIN, GPIO.LOW)
 
 def waterLevel(ref):
